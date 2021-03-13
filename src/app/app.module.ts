629dc5f52e35f87f64c21e927eb5177e82791f49
Ignas Kiaulakys <ignas.kiaulakys@gmail.com>
2021-03-13 19:24:47 +0200

    Sorted imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormComponent } from './components/form/form.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 import { MaterialModule } from './material/material.module';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
 
 const MaterialComponents = [
   MatToolbarModule,
