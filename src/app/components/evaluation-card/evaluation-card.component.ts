ad73121b38e835044ddd0b3e9160a39347c67e00
Ignas Pangonis <ignaspangonis9@gmail.com>
2021-03-22 19:45:52 +0200

    create evaluation card component

---
+++ src/app/components/evaluation-card/evaluation-card.component.ts
@@ -0,0 +1,28 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {StudentService} from '../../services/student.service';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {EvaluationCard} from '../../shared/evaluation-card';
+import {Student} from '../../shared/student';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-evaluation-card',
+  templateUrl: './evaluation-card.component.html',
+  styleUrls: ['./evaluation-card.component.scss']
+})
+export class EvaluationCardComponent implements OnInit {
+
+  public card: EvaluationCard;
+
+  constructor(private studentService: StudentService,
+              @Inject(MAT_DIALOG_DATA) public data: {stud: Student}) { }
+
+  ngOnInit(): void {
+    this.studentService.getJointEvaluation(this.data.stud.id).subscribe(
+      response => {
+        this.card = response;
+      }
+    );
+  }
+
+}
