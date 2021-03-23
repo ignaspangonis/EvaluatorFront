040c761465241eedba38494ea5af2067a363fb46
Ignas Pangonis <ignaspangonis9@gmail.com>
2021-03-23 14:53:26 +0200

    clean up

--- src/app/views/form/form.component.ts
+++ src/app/views/form/form.component.ts
@@ -2,8 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 
-import { Student } from 'src/app/shared/student';
-import { StudentService } from '../../services/student.service';
+import {Student} from 'src/app/shared/student';
+import {StudentService} from '../../services/student.service';
 import {Observable} from 'rxjs';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {Evaluation} from '../../shared/evaluation';
@@ -14,17 +14,14 @@ import {Evaluation} from '../../shared/evaluation';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-  selectedValue: string;
   profileForm: FormGroup;
   student$: Observable<Student>;
   id: string;
   isEvaluated: string;
-  evaluation$: Observable<Evaluation>;
   evaluation: Evaluation;
   evaluationId: number;
 
-  constructor(private route: ActivatedRoute, private studentService: StudentService, private fb: FormBuilder,
-              private activatedRoute: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private studentService: StudentService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -33,9 +30,9 @@ export class FormComponent implements OnInit {
         this.student$ = this.studentService.getStudentById(this.id);
       });
 
-    this.activatedRoute.queryParams.subscribe(params => {
-        this.isEvaluated = params['isEvaluated'];
-      });
+    this.route.queryParams.subscribe(params => {
+      this.isEvaluated = params['isEvaluated'];
+    });
 
     this.profileForm = this.fb.group({
       mentorID: [6],
@@ -44,26 +41,24 @@ export class FormComponent implements OnInit {
       techSkills: [''],
       learningPace: [''],
       extraMile: [''],
-      comment: [''],
-      // evaluationId: [''],
+      comment: ['']
     });
 
-    if (this.isEvaluated === 'true'){
-      this.studentService.getEvaluation(this.id).subscribe((evaluation) => {this.evaluation = evaluation,
-        this.profileForm.patchValue({mentorID: this.evaluation.mentorID,
+    if (this.isEvaluated === 'true') {
+      this.studentService.getEvaluation(this.id).subscribe((evaluation) => {
+        this.evaluation = evaluation;
+        this.profileForm.patchValue({
+          mentorID: this.evaluation.mentorID,
           studentId: this.evaluation.studentId,
-           participation: this.evaluation.participation.toString(),
-           techSkills: this.evaluation.techSkills.toString(),
-           learningPace: this.evaluation.learningPace.toString(),
-           extraMile: this.evaluation.extraMile.toString(),
-           comment: this.evaluation.comment,
-
-       });
-                                                                            this.evaluationId = this.evaluation.id;
+          participation: this.evaluation.participation.toString(),
+          techSkills: this.evaluation.techSkills.toString(),
+          learningPace: this.evaluation.learningPace.toString(),
+          extraMile: this.evaluation.extraMile.toString(),
+          comment: this.evaluation.comment,
+        });
+        this.evaluationId = this.evaluation.id;
       });
-
     }
-
   }
 
   reset() {
@@ -76,17 +71,15 @@ export class FormComponent implements OnInit {
     this.profileForm.value.techSkills = parseInt(this.profileForm.value.techSkills, 10);
     this.profileForm.value.learningPace = parseInt(this.profileForm.value.learningPace, 10);
     this.profileForm.value.extraMile = parseInt(this.profileForm.value.extraMile, 10);
-    if (this.isEvaluated === 'false'){
-    this.studentService.postEvaluation(this.profileForm.value, this.studentId.value).subscribe(() => {
-      this.reset();
-    }); }
-    else{
-      console.log(this.profileForm.value);
+    if (this.isEvaluated === 'false') {
+      this.studentService.postEvaluation(this.profileForm.value, this.studentId.value).subscribe(() => {
+        this.reset();
+      });
+    } else if (this.isEvaluated === 'true') {
       this.studentService.putEvaluation(this.profileForm.value, this.evaluationId).subscribe(() => {
         this.reset();
       });
     }
-
   }
 
   get studentId() {
