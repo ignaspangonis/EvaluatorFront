bbdbf3d36c566bcf235138197d03d7f0ebdbc0e2
Ignas Pangonis <ignaspangonis9@gmail.com>
2021-03-25 15:51:53 +0200

    clean up

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -3,11 +3,11 @@ import {Component, Input, OnInit} from '@angular/core';
 import {EvaluationCardComponent} from '../../components/evaluation-card/evaluation-card.component';
 import {EvaluationService} from '../../services/evaluation.service';
 import {MatDialog} from '@angular/material/dialog';
-import { Mentor } from 'src/app/shared/mentor';
-import { MentorService } from 'src/app/services/mentor.service';
+import {Mentor} from 'src/app/shared/mentor';
+import {MentorService} from 'src/app/services/mentor.service';
 import {Observable} from 'rxjs';
-import { Student } from 'src/app/shared/student';
-import { StudentService } from 'src/app/services/student.service';
+import {Student} from 'src/app/shared/student';
+import {StudentService} from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-home',
@@ -19,27 +19,27 @@ export class HomeComponent implements OnInit {
   students$: Observable<Student[]>;
   mentor: Mentor;
   mentorStream;
-  constructor(public dialog: MatDialog, private studentService: StudentService,  private mentorService: MentorService,
-              private evaluationService: EvaluationService) { }
+
+  constructor(public dialog: MatDialog, private studentService: StudentService, private mentorService: MentorService,
+              private evaluationService: EvaluationService) {
+  }
 
   ngOnInit(): void {
     this.getMentorStudents();
     this.getMentor();
     this.isEvaluationSaved = this.evaluationService.getIsEvaluationSaved();
-    if (this.isEvaluationSaved){
+    if (this.isEvaluationSaved) {
       setTimeout(() => {
         this.isEvaluationSaved = false;
         this.evaluationService.setIsEvaluationSaved(false);
       }, 6000);
     }
-    
-
   }
 
 
   openDialog(student: Student) {
     this.dialog.open(EvaluationCardComponent, {
-      data: { stud: student },
+      data: {stud: student},
       width: '900px',
       height: '600px'
     });
