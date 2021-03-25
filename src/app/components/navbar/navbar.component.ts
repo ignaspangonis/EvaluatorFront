3634e878a1096cb7cfa8ed805c423dd3161bca84
Simonas Liolaitis <simonas.liolaitis@gmail.com>
2021-03-25 17:39:25 +0200

    Mentor name displayed in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MentorService} from '../../services/mentor.service';
+import {Mentor} from '../../shared/mentor';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  mentor: Mentor;
+  mentorName: string;
+  constructor(private mentorService: MentorService) {  }
 
   ngOnInit(): void {
+    this.getMentor();
   }
 
+  getMentor() {
+    this.mentorService.getMentor().subscribe(
+      response => {
+        this.mentor = response;
+        this.mentorName = this.mentor.name;
+      }
+    );
+  }
 }
