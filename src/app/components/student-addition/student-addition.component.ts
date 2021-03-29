5751c1fb17207da09d8a12c3a1f6f48a1b856676
Ignas Pangonis <ignaspangonis9@gmail.com>
2021-03-29 18:43:45 +0300

    add image addition functionality

--- src/app/components/student-addition/student-addition.component.ts
+++ src/app/components/student-addition/student-addition.component.ts
@@ -3,6 +3,9 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { StudentService } from 'src/app/services/student.service';
 import {Student} from 'src/app/shared/student';
+import {HttpClient} from '@angular/common/http';
+import {take} from 'rxjs/operators';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 
 @Component({
@@ -12,9 +15,11 @@ import {Student} from 'src/app/shared/student';
 })
 export class StudentAdditionComponent implements OnInit {
   studentAdditionForm: FormGroup;
+  selectedFile = null;
+  imageUrl: SafeResourceUrl = 'https://gladstoneentertainment.com/wp-content/uploads/2018/08/blank-portrait.png';
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: {str: string}, private fb: FormBuilder, private studentService: StudentService,
-  public dialogRef: MatDialogRef<StudentAdditionComponent>) {
+              public dialogRef: MatDialogRef<StudentAdditionComponent>, private httpClient: HttpClient, private sanitizer: DomSanitizer) {
     dialogRef.disableClose = true;
    }
 
@@ -25,10 +30,6 @@ export class StudentAdditionComponent implements OnInit {
     });
   }
 
-  close(){
-    this.dialogRef.close(null);
-  }
-
   get name() {
     return this.studentAdditionForm.get('name') as FormControl;
   }
@@ -36,5 +37,29 @@ export class StudentAdditionComponent implements OnInit {
   get surname() {
     return this.studentAdditionForm.get('surname') as FormControl;
   }
-  
+
+  close() {
+    this.dialogRef.close(null);
+  }
+
+  onFileSelected(event) {
+    const selectedFile = event.target.files[0];
+    if (selectedFile !== null) {
+      this.selectedFile = selectedFile;
+      this.imageUrl = this.sanitizer.bypassSecurityTrustResourceUrl((URL).createObjectURL(this.selectedFile));
+    }
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('upload_preset', 'evaluator-upload');
+    formData.append('file', this.selectedFile);
+    this.httpClient.post('https://api.cloudinary.com/v1_1/ignaspan/upload', formData, {
+      observe: 'events'
+    })
+      .subscribe(event => {
+        console.log(JSON.parse(JSON.stringify(event)).body.url);
+      });
+  }
+
 }
